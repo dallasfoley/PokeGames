cf0f46115b5deda032a90e7e16fe749b30bec7d8
dallasfoley <dallastfoley@gmail.com>
2024-03-25 11:49:39 -0400

    progress on fetching data logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Row, { PokemonApi } from "./Row";
 
 function App() {
   const [input, setInput] = useState("");
-  const [madeFirstGuess, setMadeFirstGuess] = useState(false);
+  const [guessCount, setGuessCount] = useState(0);
   const [guesses, setGuesses] = useState<PokemonApi[]>([]);
+  const [answer, setAnswer] = useState<PokemonApi | null>(null);
 
-  const getPokemon = async () => {
-    setMadeFirstGuess(true);
+  const handleGuess = async () => {
+    setGuessCount(guessCount + 1);
     try {
       const rawData = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`);
       const data = await rawData.json();
@@ -16,15 +17,16 @@ function App() {
       const speciesUrl = data.species.url;
       const speciesRawData = await fetch(speciesUrl);
       const speciesData = await speciesRawData.json();
+
       const pokemonData = {
         name: data.name,
         type1: data.types.length > 0 ? data.types[0].type.name : "None",
         type2: data.types.length > 1 ? data.types[1].type.name : "None",
-        habitat: speciesData.habitat ? speciesData.habitat.name : "Unknown", // Placeholder for now
-        color: speciesData.color ? speciesData.color.name : "Unknown", // Placeholder for now
+        habitat: speciesData.habitat ? speciesData.habitat.name : "Unknown",
+        color: speciesData.color ? speciesData.color.name : "Unknown",
         evolutionStage: "Unknown", // Placeholder, determining this would require additional logic
-        height: `${data.height * 10} cm`, // Decimeters to meters might need conversion
-        weight: `${data.weight / 10} kg`, // Hectograms to kilograms might need conversion
+        height: `${data.height * 10} cm`,
+        weight: `${data.weight / 10} kg`,
       };
       setGuesses([...guesses, pokemonData]);
     } catch (error) {
@@ -32,6 +34,23 @@ function App() {
       alert("Failed to fetch Pokémon");
     }
   };
+
+  useEffect(() => {
+    const getAnswer = async () => {
+      const id = Math.floor(Math.random() * 1025);
+      try {
+        const rawData = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await rawData.json();
+        setAnswer(data);
+      } catch (error) {
+        console.error(error);
+        alert("Failed to fetch Answer Pokémon, reload page");
+      }
+    };
+    getAnswer();
+  }, []);
+
+  console.log(`answer: ${answer?.name}`);
 
   return (
     <>
@@ -42,9 +61,9 @@ function App() {
             onChange={(e) => setInput(e.target.value)}
             placeholder="Type Pokemon name..."
           ></input>
-          <button onClick={() => getPokemon()}>Guess</button>
-          {madeFirstGuess ? null : <div>Guess a pokemon to begin</div>}
-          {madeFirstGuess && (
+          <button onClick={() => handleGuess()}>Guess</button>
+          {guessCount === 0 ? null : <div>Guess a pokemon to begin</div>}
+          {guessCount > 0 && (
             <div className="attributes-header">
               <div>Name</div>
               <div>Type 1</div>
