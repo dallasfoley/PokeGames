0bbbfda94f4f2f8192ea5035e64ebd0bad904cb7
dallasfoley <dallastfoley@gmail.com>
2024-03-25 18:28:43 -0400

    state logic

--- src/App.tsx
+++ src/App.tsx
@@ -11,80 +11,83 @@ function App() {
   const [state, setState] = useState(false);
 
   const handleGuess = async () => {
-    try {
-      const rawData = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`);
-      const data = await rawData.json();
-      setGuessCount(guessCount + 1);
-      const speciesUrl = data.species.url;
-      const speciesRawData = await fetch(speciesUrl);
-      const speciesData = await speciesRawData.json();
+    if (state === false) {
+      try {
+        const rawData = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${input}`
+        );
+        const data = await rawData.json();
+        setGuessCount(guessCount + 1);
+        const speciesUrl = data.species.url;
+        const speciesRawData = await fetch(speciesUrl);
+        const speciesData = await speciesRawData.json();
 
-      const pokemonData = {
-        name: data.name,
-        type1: data.types.length > 0 ? data.types[0].type.name : "None",
-        type2: data.types.length > 1 ? data.types[1].type.name : "None",
-        habitat: speciesData.habitat ? speciesData.habitat.name : "Unknown",
-        color: speciesData.color ? speciesData.color.name : "Unknown",
-        evolutionStage: "Unknown", // Placeholder, determining this would require additional logic
-        height: `${data.height * 10} cm`,
-        weight: `${data.weight / 10} kg`,
-        isCorrect: [
-          answer && data.name === answer.name ? true : false,
-          answer && data.types[0].type.name === answer.type1 ? true : false,
-          answer &&
-          data.types.length > 1 &&
-          data.types[1].type.name === answer.type2
-            ? true
-            : false,
-          answer && speciesData.habitat.name === answer.habitat ? true : false,
-          answer && speciesData.color.name === answer.color ? true : false,
-          false,
-          answer && `${data.height * 10} cm` === answer.height ? true : false,
-          answer && `${data.weight / 10} kg` === answer.weight ? true : false,
-        ],
-      };
+        const pokemonData = {
+          name: data.name,
+          type1: data.types.length > 0 ? data.types[0].type.name : "None",
+          type2: data.types.length > 1 ? data.types[1].type.name : "None",
+          habitat: speciesData.habitat ? speciesData.habitat.name : "Unknown",
+          color: speciesData.color ? speciesData.color.name : "Unknown",
+          evolutionStage: "Unknown", // Placeholder, determining this would require additional logic
+          height: `${data.height * 10} cm`,
+          weight: `${data.weight / 10} kg`,
+          isCorrect: [
+            answer && data.name === answer.name ? true : false,
+            answer && data.types[0].type.name === answer.type1 ? true : false,
+            answer &&
+            data.types.length > 1 &&
+            data.types[1].type.name === answer.type2
+              ? true
+              : false,
+            answer && speciesData.habitat.name === answer.habitat
+              ? true
+              : false,
+            answer && speciesData.color.name === answer.color ? true : false,
+            false,
+            answer && `${data.height * 10} cm` === answer.height ? true : false,
+            answer && `${data.weight / 10} kg` === answer.weight ? true : false,
+          ],
+        };
 
-      setGuesses([pokemonData, ...guesses]);
-      answer && data.name === answer.name && setState(true);
-    } catch (error) {
-      console.error(error);
-      alert("Failed to fetch Pokémon");
+        setGuesses([pokemonData, ...guesses]);
+        answer && data.name === answer.name && setState(true);
+      } catch (error) {
+        console.error(error);
+        alert("Failed to fetch Pokémon");
+      }
     }
   };
 
   useEffect(() => {
     const getAnswer = async () => {
       const id = Math.floor(Math.random() * 151);
-      if (state === false) {
-        try {
-          const rawData = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/${id}`
-          );
-          const data = await rawData.json();
-          const speciesUrl = data.species.url;
-          const speciesRawData = await fetch(speciesUrl);
-          const speciesData = await speciesRawData.json();
-          const pokemonData = {
-            name: data.name,
-            type1: data.types.length > 0 ? data.types[0].type.name : "None",
-            type2: data.types.length > 1 ? data.types[1].type.name : "None",
-            habitat: speciesData.habitat ? speciesData.habitat.name : "Unknown",
-            color: speciesData.color ? speciesData.color.name : "Unknown",
-            evolutionStage: "Unknown", // Placeholder, determining this would require additional logic
-            height: `${data.height * 10} cm`,
-            weight: `${data.weight / 10} kg`,
-            isCorrect: [false],
-          };
-          setAnswer(pokemonData);
-          console.log(pokemonData.name);
-        } catch (error) {
-          console.error(error);
-          alert("Failed to fetch Answer Pokémon, reload page");
-        }
+      try {
+        const rawData = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await rawData.json();
+        const speciesUrl = data.species.url;
+        const speciesRawData = await fetch(speciesUrl);
+        const speciesData = await speciesRawData.json();
+        const pokemonData = {
+          name: data.name,
+          type1: data.types.length > 0 ? data.types[0].type.name : "None",
+          type2: data.types.length > 1 ? data.types[1].type.name : "None",
+          habitat: speciesData.habitat ? speciesData.habitat.name : "Unknown",
+          color: speciesData.color ? speciesData.color.name : "Unknown",
+          evolutionStage: "Unknown", // Placeholder, determining this would require additional logic
+          height: `${data.height * 10} cm`,
+          weight: `${data.weight / 10} kg`,
+          isCorrect: [false],
+        };
+        setAnswer(pokemonData);
+        console.log(pokemonData.name);
+      } catch (error) {
+        console.error(error);
+        alert("Failed to fetch Answer Pokémon, reload page");
       }
     };
     getAnswer();
-  }, []);
+    setState(false);
+  }, [state]);
 
   return (
     <>
