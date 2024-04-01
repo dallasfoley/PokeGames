8a9b471def8443432a4a639d1cc0eb8804454a59
Mia Fallon <pop37corn@gmail.com>
2024-04-01 18:46:42 -0400

    Refactor

--- src/pages/Classic.tsx
+++ src/pages/Classic.tsx
@@ -1,180 +1,156 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useContext } from "react";
 import "../App.css";
 import Row from "../components/Row";
 import { PokemonApi } from "../components/Row";
-import { useContext } from "react";
 import { ThemeContext } from "../App";
+
+const isDev = import.meta.env.MODE === "development";
+
+const API_BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+const ColumnHead = {
+  name: "Name",
+  type1: "Type 1",
+  type2: "Type 2",
+  habitat: "Habitat",
+  color: "Color",
+  stage: "Stage",
+  height: "Height",
+  weight: "Weight",
+};
+
+const getPokemonData = async (url: string, answer: PokemonApi | null = null) => {
+  const pokemonResponse = await fetch(url);
+  const pokemon = await pokemonResponse.json();
+
+  const speciesResponse = await fetch(pokemon.species.url);
+  const species = await speciesResponse.json();
+
+  const evolutionChainResponse = await fetch(species.evolution_chain.url);
+  const evolutionChain = await evolutionChainResponse.json();
+
+  const {name, type1, type2, habitat, color, evolutionStage, height, weight} = answer || {};
+
+  const newEvoState =
+    (species.evolves_from_species ? 1 : 0) +
+    (evolutionChain.chain.evolves_to.length > 0 ? 1 : 0) +
+    (evolutionChain.chain.evolves_to[0].evolves_to.length > 0 ? 1 : 0)
+      .toString();
+
+  return {
+    name: pokemon.name,
+    type1: pokemon.types[0].type.name,
+    type2: pokemon.types[1]?.type.name || "None",
+    habitat: species.habitat?.name || "Unknown",
+    color: species.color?.name || "Unknown",
+    evolutionStage: newEvoState,
+    height: `${pokemon.height * 10} cm`,
+    weight: `${pokemon.weight / 10} kg`,
+    picUrl: pokemon.sprites.front_default,
+    isCorrect: [
+      pokemon.name === name,
+      pokemon.types[0].type.name === type1,
+      (pokemon.types[1]?.type.name === type2 ||
+              type2 === "None") && species.habitat?.name === habitat,
+      species.habitat?.name === habitat,
+      species.color?.name === color,
+      newEvoState === evolutionStage,
+      `${pokemon.height * 10} cm` === height,
+      `${pokemon.weight / 10} kg` === weight,
+    ],
+  } as PokemonApi;
+};
 
 const Classic = () => {
   const [input, setInput] = useState("");
   const [guesses, setGuesses] = useState<PokemonApi[]>([]);
   const [answer, setAnswer] = useState<PokemonApi | null>(null);
-  const [answerPic, setAnswerPic] = useState("");
-  const [state, setState] = useState(false);
   const darkTheme = useContext(ThemeContext).darkTheme;
-  const renderCountRef = useRef(0);
+  const randomPokemonId = Math.floor(Math.random() * 151);
 
-  const getPokemonData = async (url: string) => {
-    const rawData = await fetch(url);
-    const data = await rawData.json();
-    const speciesUrl = data.species.url;
-    const speciesRawData = await fetch(speciesUrl);
-    const speciesData = await speciesRawData.json();
-    const evolutionChainUrl = speciesData.evolution_chain.url;
-    const evolutionChainRawData = await fetch(evolutionChainUrl);
-    const evolutionChainData = await evolutionChainRawData.json();
-    renderCountRef.current === 0 && setAnswerPic(data.sprites.front_default);
-    renderCountRef.current = renderCountRef.current + 1;
-    let evolutionStage;
-    const evolvesFrom = speciesData.evolves_from_species;
-    const firstEvolution = evolutionChainData.chain;
-
-    if (!evolvesFrom && firstEvolution.evolves_to.length > 0) {
-      evolutionStage = "1";
-    } else if (
-      evolvesFrom &&
-      firstEvolution.evolves_to.length > 0 &&
-      firstEvolution.evolves_to[0].evolves_to.length > 0
-    ) {
-      evolutionStage = "2";
-    } else {
-      evolutionStage = "3";
-    }
-    const pokemonData = {
-      name: data.name,
-      type1: data.types.length > 0 ? data.types[0].type.name : "None",
-      type2: data.types.length > 1 ? data.types[1].type.name : "None",
-      habitat: speciesData.habitat ? speciesData.habitat.name : "Unknown",
-      color: speciesData.color ? speciesData.color.name : "Unknown",
-      evolutionStage: evolutionStage,
-      height: `${data.height * 10} cm`,
-      weight: `${data.weight / 10} kg`,
-      isCorrect:
-        guesses.length === 0
-          ? [false]
-          : [
-              answer && data.name === answer.name ? true : false,
-              answer && data.types[0].type.name === answer.type1 ? true : false,
-              (answer &&
-                data.types.length > 1 &&
-                data.types[1].type.name === answer.type2) ||
-              (data.types.length === 1 && answer?.type2 === "None")
-                ? true
-                : false,
-              answer && speciesData.habitat.name === answer.habitat
-                ? true
-                : false,
-              answer && speciesData.color.name === answer.color ? true : false,
-              answer && evolutionStage === answer.evolutionStage ? true : false,
-              answer && `${data.height * 10} cm` === answer.height
-                ? true
-                : false,
-              answer && `${data.weight / 10} kg` === answer.weight
-                ? true
-                : false,
-            ],
-    };
-    return pokemonData;
-  };
+  const hasWon = guesses.length > 0 && guesses[0].name === answer?.name;
 
   const handleGuess = async () => {
-    if (state === false) {
-      try {
+    try {
+      if(answer === null || hasWon) {
+        setGuesses([]);
         const data = await getPokemonData(
-          `https://pokeapi.co/api/v2/pokemon/${input.toLowerCase()}`
+          `${API_BASE_URL}${randomPokemonId}`,
+          answer
+        );
+        isDev && console.log("answer: ", data.name);
+        setAnswer(data);
+      }
+      if(!hasWon && input !== "") {
+        const data = await getPokemonData(
+          `${API_BASE_URL}${input.toLowerCase()}`,
+          answer
         );
         setGuesses([data, ...guesses]);
-        answer && data.name === answer.name && setState(true);
-      } catch (error) {
-        console.error(error);
-        alert("Failed to fetch Pokémon");
       }
+    } catch (error) {
+      alert("Failed to fetch Pokémon");
+    } finally {
+      setInput("");
     }
   };
 
-  useEffect(() => {
-    const getAnswer = async () => {
-      const id = Math.floor(Math.random() * 151);
-      try {
-        const data = await getPokemonData(
-          `https://pokeapi.co/api/v2/pokemon/${id}`
-        );
-        setAnswer(data);
-        console.log(data.name);
-      } catch (error) {
-        console.error(error);
-        alert("Failed to fetch Answer Pokémon, reload page");
-      }
-    };
-    getAnswer();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
-    <>
-      <div
-        className="Classic"
-        style={{ backgroundColor: darkTheme ? "#2f3133" : "#f0f0f0" }}
-      >
-        <div className="wrapper">
-          <div className="input-group">
-            <input
-              className="guess-input"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="Type Pokemon name..."
-              onKeyDown={(e) => e.key === "Enter" && handleGuess()}
-              style={{
-                background: darkTheme ? "#ebfffc" : "#2f3133",
-                color: darkTheme ? "#2f3133" : "#f0f0f0",
-              }}
-            ></input>
-            <button
-              className="guess-button"
-              onClick={() => handleGuess()}
-              style={{
-                color: darkTheme ? "#2f3133" : "#ebfffc",
-                background: darkTheme ? "#ebfffc" : "#2f3133",
-              }}
-            >
-              Guess
-            </button>
+    <div
+      style={{ backgroundColor: darkTheme ? "#2f3133" : "#f0f0f0" }}
+    >
+      <div className="wrapper">
+        <div className="input-group">
+          <input
+            className="guess-input"
+            disabled={hasWon}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type Pokemon name..."
+            onKeyDown={(e) => e.key === "Enter" && handleGuess()}
+            style={{
+              background: darkTheme ? "#ebfffc" : "#2f3133",
+              color: darkTheme ? "#2f3133" : "#f0f0f0",
+            }}
+          />
+          <button
+            className="guess-button"
+            onClick={handleGuess}
+            style={{
+              color: darkTheme ? "#2f3133" : "#ebfffc",
+              background: darkTheme ? "#ebfffc" : "#2f3133",
+            }}
+          >
+            {hasWon ? "Reset" : "Guess"}
+          </button>
+        </div>
+        {hasWon && (
+          <div className="win">
+            Congratulations! It took you {guesses.length} guess{guesses.length !== 1 && "es"} to correctly guess
+            the Pokemon!
+            <img className="classic-img" src={answer?.picUrl} />
           </div>
-          {state && (
-            <div className="win">
-              Congratulations! It took you {guesses.length}{" "}
-              {guesses.length === 1 ? "guess" : "guesses"} to correctly guess
-              the Pokemon!
-              <img className="classic-img" src={answerPic}></img>
-            </div>
-          )}
-          {guesses.length > 0 ? (
-            <div className="attributes-header">
-              <div>Name</div>
-              <div>Type 1</div>
-              <div>Type 2</div>
-              <div>Habitat</div>
-              <div>Color</div>
-              <div>Stage</div>
-              <div>Height</div>
-              <div>Weight</div>
-            </div>
-          ) : (
-            <div
-              style={{
-                fontSize: "20px",
-                color: darkTheme ? "#ffffff" : "#2f3133",
-              }}
-            >
-              Guess a Pokemon to begin
-            </div>
-          )}
-          {guesses.map((guess) => (
-            <Row {...guess} key={guess.name} />
+        )}
+        <div className="attributes-header">
+          {Object.entries(ColumnHead).map(([key, val]) => (
+            <div key={key}>{val}</div>
           ))}
         </div>
+        {guesses.length === 0 &&
+          <p
+            style={{
+              fontSize: "20px",
+              color: darkTheme ? "#ffffff" : "#2f3133",
+            }}
+          >
+            Guess a Pokemon to begin
+          </p>
+        }
+        {guesses.map((guess, idx) => (
+          <Row key={`${idx}-${guess.name}`} {...guess} />
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
